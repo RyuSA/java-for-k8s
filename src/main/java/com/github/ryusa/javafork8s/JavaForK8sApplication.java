df2ff587fa1fb88f020d552a00614b4db262cfe8
RyuSA <12961775+RyuSA@users.noreply.github.com>
2022-12-03 00:00:29 +0900

    init: spring boot project

---
+++ src/main/java/com/github/ryusa/javafork8s/JavaForK8sApplication.java
@@ -0,0 +1,11 @@
+package com.github.ryusa.javafork8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaForK8sApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavaForK8sApplication.class, args);
+	}
+}
